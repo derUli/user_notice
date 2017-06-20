6d3caa4c50b6541a673a80047e2bd55fa9194156
Ulrich Schmidt <admin@deruli.de>
2017-06-20 21:46:45 +0200

    post-install script verschoben, uninstall methode korrigiert

--- src/content/modules/user_notice/UserNotice.php
+++ src/content/modules/user_notice/UserNotice.php
@@ -2,7 +2,7 @@
 class UserNotice extends Controller {
 	private $moduleName = "user_notice";
 	public function uninstall() {
-		$migrator = new DBMigrator ( $this->moduleName, "lib/updates/down" );
+		$migrator = new DBMigrator ( $this->moduleName, ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "migrations/down" ) );
 		$migrator->rollback ();
 	}
 }
