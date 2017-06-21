559f65217bf2e742f3f832167dfc93ddc7ae52ab
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-21 11:00:06 +0200

    unit test

---
+++ src/tests/UserNoticeDataTest.php
@@ -0,0 +1,29 @@
+<?php
+class UserNoticeDataTest extends PHPUnit_Framework_TestCase {
+	const originalString = "<p>test<script>alert('xss');</script></p>";
+	const filteredString = "<p>test</p>";
+	public function setUp() {
+		$sql = "update {prefix}users set notice = ? where notice = ? or notice = ?";
+		$args = array (
+				null,
+				self::originalString,
+				self::filteredString 
+		);
+		Database::pQuery ( $sql, $args, true );
+	}
+	public function tearDown() {
+		$this->setUp ();
+	}
+	public function testSetUserNotice() {
+		$user = new UserNoticeData ();
+		$user->loadByUsername ( "admin" );
+		$this->assertNotNull ( $user->getId () );
+		$this->assertNull ( $user->getNotice () );
+		$user->setNotice ( self::originalString );
+		$this->assertEquals ( self::filteredString, $user->getNotice () );
+		$this->save ();
+		$user = new UserNoticeData ();
+		$user->loadByUsername ( "admin" );
+		$this->assertEquals ( self::filteredString, $user->getNotice () );
+	}
+}
