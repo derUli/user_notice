6041b3dc5613b285bfa76f36d0da26a6cfc0c201
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-21 10:24:39 +0200

    UserNoticeData erstellt

--- src/content/modules/user_notice/UserNotice.php
+++ src/content/modules/user_notice/UserNotice.php
@@ -1,5 +1,5 @@
 <?php
-class UserNotice extends Controller {
+class UserNoticeModule extends Controller {
 	private $moduleName = "user_notice";
 	public function uninstall() {
 		$migrator = new DBMigrator ( $this->moduleName, ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "migrations/down" ) );
---
+++ src/content/modules/user_notice/objects/UserNoticeData.php
@@ -0,0 +1,20 @@
+<?php
+class UserNoticeData extends User {
+	protected $notice = null;
+	const allowableTags = "<a><abbr><address><area><article><aside><audio><b><bdi><bdo><blockquote><br/><br><button><canvas><caption><cite><code><col><colgroup><command><data><datalist><dd><del><details><dfn><div><dl><dt><em><embed><fieldset><figcaption><figure><font><footer><form><header><hgroup><hr><i><iframe><img><input><ins><kbd><keygen><label><legend><li><map><mark><math><menu><meter><nav><object><ol><optgroup><option><output><p><param><pre><progress><q><rp><rt><ruby><s><samp><section><select><small><source><span><strong><sub><summary><sup><svg><table><tbody><td><textarea><tfoot><th><thead><time><tr><track><u><ul><var><video><wbr>";
+	public function getNotice() {
+		return $this->notice;
+	}
+	public function setNotice($val) {
+		if (is_string ( $val )) {
+			$trimmed = trim ( strip_tags ( $val, "<img><table><a>" ) );
+			if (StringHelper::isNotNullOrWhitespace ( $trimmed )) {
+				$this->notice = strip_tags ( $val, self::allowableTags );
+			} else {
+				$this->notice = null;
+			}
+		} else {
+			$this->notice = null;
+		}
+	}
+}
