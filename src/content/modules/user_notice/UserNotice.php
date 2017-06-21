773b2241acc69f90dcc3f3ab5963b01d74d766d3
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-21 11:05:17 +0200

    klasse wieder umbenannt

--- src/content/modules/user_notice/UserNotice.php
+++ src/content/modules/user_notice/UserNotice.php
@@ -1,5 +1,5 @@
 <?php
-class UserNoticeModule extends Controller {
+class UserNotice extends Controller {
 	private $moduleName = "user_notice";
 	public function uninstall() {
 		$migrator = new DBMigrator ( $this->moduleName, ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "migrations/down" ) );
