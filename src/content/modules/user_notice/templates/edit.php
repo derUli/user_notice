3eb84cec67ec712d9f1ee0b8c3e7364923cde5c0
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-22 13:45:12 +0200

    if-schleifen gedönse

--- src/content/modules/user_notice/templates/edit.php
+++ src/content/modules/user_notice/templates/edit.php
@@ -1,15 +1,18 @@
 <?php
 $acl = new ACL ();
 if ($acl->hasPermission ( "user_notice" )) {
+	$user = new UserNoticeData ( get_user_id () );
+	$note = $user->getNotice () ? strip_tags ( $user->getNotice (), UserNoticeData::allowableTags ) : "";
 	
 	?>
 <form action="#" method="post">
 	<p>
-		<textarea></textarea>
+		<textarea><?php Template::escape($note);?></textarea>
 	</p>
 	<p>
 		<input type="submit" value="<?php translate("save");?>">
 	</p>
+	<!-- @TODO: CKEditor oder CodeMirror für Textarea starten -->
 </form>
 <?php
 } else {
