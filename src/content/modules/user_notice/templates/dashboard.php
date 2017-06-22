9fd9adce52806945acb48787547c04f0bac23ef4
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-22 13:55:33 +0200

    formular

--- src/content/modules/user_notice/templates/dashboard.php
+++ src/content/modules/user_notice/templates/dashboard.php
@@ -3,9 +3,7 @@ $acl = new ACL ();
 if ($acl->hasPermission ( "user_notice" )) {
 	$user = new UserNoticeData ( get_user_id () );
 	$note = $user->getNotice () ? strip_tags ( $user->getNotice (), UserNoticeData::allowableTags ) : "";
-	?>
-
-<h2 class="accordion-header"><?php translate("my_notes");?></h2>
+	?><h2 class="accordion-header" id="tab-notes"><?php translate("my_notes");?></h2>
 <div class="accordion-content">
 <?php
 	if (StringHelper::isNotNullOrWhitespace ( $note )) {
@@ -17,4 +15,7 @@ if ($acl->hasPermission ( "user_notice" )) {
 			href="<?php Template::escape(ModuleHelper::buildActionURL("edit_notice"))?>"><?php translate("edit");?></a>]
 	</p>
 </div>
+<?php if(Request::getVar("tab") == "notes"){?>
+<script type="text/javascript">$("#tab-notes").click();</script>
+<?php }?>
 <?php }?>
--- src/content/modules/user_notice/templates/edit.php
+++ src/content/modules/user_notice/templates/edit.php
@@ -5,7 +5,7 @@ if ($acl->hasPermission ( "user_notice" )) {
 	$note = $user->getNotice () ? strip_tags ( $user->getNotice (), UserNoticeData::allowableTags ) : "";
 	?>
 <form
-	action="<?php Template::escape(ModuleHelper::buildActionURL("home", "sClass=UserNotice&sMethod=save&tab=notice"));?>"
+	action="<?php Template::escape(ModuleHelper::buildActionURL("home", "sClass=UserNotice&sMethod=save&tab=notes"));?>"
 	method="post">
 	<p>
 		<textarea name="my_notes" id="my_notes"><?php Template::escape($note);?></textarea>
