fce93b34d35490e656d4f30b7906888957829a4a
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-22 14:09:44 +0200

    headline added

--- src/content/modules/user_notice/UserNotice.php
+++ src/content/modules/user_notice/UserNotice.php
@@ -9,6 +9,10 @@ class UserNotice extends Controller {
 		echo Template::executeModuleTemplate ( $this->moduleName, "dashboard.php" );
 	}
 	public function save() {
+		$acl = new ACL ();
+		if (Request::getMethod () != "post" or ! is_logged_in () or ! $acl->hasPermission ( "user_notice" )) {
+			Request::redirect ( ModuleHelper::buildActionURL ( "edit_notice" ) );
+		}
 		// @TODO: Speichern implementieren
 		Request::redirect ( ModuleHelper::buildActionURL ( "home", "tab=notes" ) );
 	}
