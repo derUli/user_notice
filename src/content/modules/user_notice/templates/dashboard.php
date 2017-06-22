26c2c3cca45996158269f6a4f9cdce0bc5440662
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-22 13:44:03 +0200

    if-schleifen gedönse

--- src/content/modules/user_notice/templates/dashboard.php
+++ src/content/modules/user_notice/templates/dashboard.php
@@ -2,7 +2,7 @@
 $acl = new ACL ();
 if ($acl->hasPermission ( "user_notice" )) {
 	$user = new UserNoticeData ( get_user_id () );
-	$note = strip_tags ( $user->getNotice (), UserNoticeData::allowableTags );
+	$note = $user->getNotice () ? strip_tags ( $user->getNotice (), UserNoticeData::allowableTags ) : "";
 	?>
 
 <h2 class="accordion-header"><?php translate("my_notes");?></h2>
