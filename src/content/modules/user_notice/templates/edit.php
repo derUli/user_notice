30da99a1083495aa1090cab7b61fd00403bc85e8
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-22 13:50:53 +0200

    formular

--- src/content/modules/user_notice/templates/edit.php
+++ src/content/modules/user_notice/templates/edit.php
@@ -3,15 +3,17 @@ $acl = new ACL ();
 if ($acl->hasPermission ( "user_notice" )) {
 	$user = new UserNoticeData ( get_user_id () );
 	$note = $user->getNotice () ? strip_tags ( $user->getNotice (), UserNoticeData::allowableTags ) : "";
-	
 	?>
-<form action="#" method="post">
+<form
+	action="<?php Template::escape(ModuleHelper::buildActionURL("home", "sClass=UserNotice&sMethod=save&tab=notice"));?>"
+	method="post">
 	<p>
 		<textarea name="my_notes" id="my_notes"><?php Template::escape($note);?></textarea>
 	</p>
 	<p>
 		<input type="submit" value="<?php translate("save");?>">
 	</p>
+	<?php csrf_token_html();?>
 	<!-- @TODO: CKEditor oder CodeMirror für Textarea starten -->
 </form>
 <?php
